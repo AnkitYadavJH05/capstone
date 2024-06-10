7edbed2cf5b717812883587f6a6b631cb170af11
ANKIT KUMAR YADAV <139650138+AnkitYadavJH05@users.noreply.github.com>
2024-06-10 15:31:08 +0530

    Update userController.js

--- BACKEND/routes/controllers/userController.js
+++ BACKEND/routes/controllers/userController.js
@@ -89,7 +89,7 @@ export const login = async (req, res) => {
         });
     }
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error from catch" });
   }
 };
 
