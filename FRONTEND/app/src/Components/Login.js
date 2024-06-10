0aab957e43c5f0dc5dfe0eee373d54b35857128a
ANKIT KUMAR YADAV <139650138+AnkitYadavJH05@users.noreply.github.com>
2024-06-10 16:48:00 +0530

    Update Login.js

--- FRONTEND/app/src/Components/Login.js
+++ FRONTEND/app/src/Components/Login.js
@@ -39,7 +39,7 @@ function Login() {
         toast.success('Login Success');
         setTimeout(()=>{
           window.location.reload()
-        },20000)
+        },2000)
         navigate('/')
       }else if(res.data.status === 403){
           setError(res.data.message)
