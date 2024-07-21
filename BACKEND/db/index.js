dafe6e82e83886b332ccd374bc4926fefac0f2e4
ANKIT KUMAR YADAV <139650138+AnkitYadavJH05@users.noreply.github.com>
2024-07-21 11:58:05 +0530

    Update index.js

--- BACKEND/db/index.js
+++ BACKEND/db/index.js
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 
 config()
 const connection = mongoose.connect(
-  "mongodb+srv://ankryadav12:Dream11@flixxitproject.emigslu.mongodb.net/?retryWrites=true&w=majority&appName=FlixxitProject",
+  "mongodb+srv://ankityadav:Dream11@project.rgjegji.mongodb.net/?retryWrites=true&w=majority&appName=project",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
